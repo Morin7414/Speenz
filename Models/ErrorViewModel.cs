551a515695688c23d414180c2612c61d2afd503e
Dean Morin <morin7414@gmail.com>
2022-07-08 23:36:40 -0600

    This is my first website

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Speeenz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
